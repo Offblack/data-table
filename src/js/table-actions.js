560c9cce8982de07d0a5afcb54dd87e73c7f4c86
Michal Opalka <michalopalka998@gmail.com>
2019-09-21 14:54:26 +0200

    Working convertion of data

--- src/js/table-actions.js
+++ src/js/table-actions.js
@@ -1,27 +1,35 @@
-//Generate Table Head
-export const generateTableHead = data => {
-   let table = document.createElement('table');
-   let thead = table.createTHead();
-   let row = thead.insertRow();
-   for (let key of data) {
-      let th = document.createElement('th');
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
-   }
-   document.body.appendChild(table);
-};
-
-//Generate Table
-export const generateTable = data => {
-   let table = document.createElement('table');
-   for (let element of data) {
-      let row = table.insertRow();
-      for (let key in element) {
+const generateData = (row, element) => {
+   for (let key in element) {
+      if (key === 'id') row.dataset.key = element[key];
+      else {
          let cell = row.insertCell();
-         let text = document.createTextNode(element[key]);
+         let text = '';
+         if (element[key] === true) text = document.createTextNode('Yes');
+         else if (element[key] === false) text = document.createTextNode('No');
+         else text = document.createTextNode(element[key]);
          cell.appendChild(text);
       }
    }
-   document.body.appendChild(table);
+};
+
+//Generate Table Head
+export const generateTableHead = (table, data) => {
+   let thead = table.createTHead();
+   let row = thead.insertRow();
+   for (let key of data) {
+      if (key != 'id') {
+         let th = document.createElement('th');
+         let text = document.createTextNode(key);
+         th.appendChild(text);
+         row.appendChild(th);
+      }
+   }
+};
+
+//Generate Table
+export const generateTable = (table, data) => {
+   for (let element of data) {
+      let row = table.insertRow();
+      generateData(row, element);
+   }
 };
--- src/js/table-structure.js
+++ src/js/table-structure.js
@@ -1,7 +1,12 @@
 import { users } from '../data/data';
 import { generateTableHead, generateTable } from './table-actions';
 
-let data = Object.keys(users[0]);
+const tableInit = () => {
+   let data = Object.keys(users[0]);
+   let table = document.createElement('table');
+   generateTableHead(table, data);
+   generateTable(table, users);
+   document.body.appendChild(table);
+};
 
-generateTableHead(data);
-generateTable(users);
+tableInit();
